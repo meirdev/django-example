e99728f6e6a5a0b8d2b5e999eac94e0596ac767a
meirdev <maornet@gmail.com>
2023-02-13 16:04:34 +0200

    fix type

--- app/settings.py
+++ app/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(strtobool(os.environ.get("DEBUG", False)))
+DEBUG = bool(strtobool(os.environ.get("DEBUG", "False")))
 
 ALLOWED_HOSTS = [
     "*",
